8c604795383fb76967f0ab4b2a47b7b5371a52a4
“JoanRoucoux” <joan.roucoux@gmail.com>
2022-11-11 15:02:05 +0100

    [EVO] Add storybook configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import { CacheProvider, EmotionCache } from '@emotion/react';
-import { CssBaseline } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import createEmotionCache from '../src/config/createEmotionCache';
-import theme from '../src/config/theme';
+import createEmotionCache from '../config/createEmotionCache';
+import theme from '../config/theme';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -13,7 +13,7 @@ interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
 
-const MyApp = (props: MyAppProps) => {
+export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
     <CacheProvider value={emotionCache}>
@@ -21,12 +21,9 @@ const MyApp = (props: MyAppProps) => {
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
   );
-};
-
-export default MyApp;
+}
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import createEmotionCache from './config/createEmotionCache';
-import theme from './config/theme';
-
-export { createEmotionCache, theme };
