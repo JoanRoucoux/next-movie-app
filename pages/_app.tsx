e6afae9a91f88f353a4880b8fc07bad749b99513
“JoanRoucoux” <joan.roucoux@gmail.com>
2022-11-11 15:19:55 +0100

    [REVERT] Revert base configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,6 @@
-import { CacheProvider, EmotionCache } from '@emotion/react';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import createEmotionCache from '../config/createEmotionCache';
-import theme from '../config/theme';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-// Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache();
-
-interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache;
-}
-
-export default function MyApp(props: MyAppProps) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-  return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
